63e2809775f7c98a44320ccb70b33299650f28e5
Julien <julien.krause@protonmail.com>
2024-02-16 22:10:34 +0100

    Created Project-structure and sample project.

---
+++ src/main/java/de/esterlino/TimeplaterApplication.java
@@ -0,0 +1,17 @@
+package de.esterlino;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class TimeplaterApplication {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My App");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        SwingUtilities.invokeLater(() -> {
+            frame.setVisible(true);
+        });
+    }
+}
