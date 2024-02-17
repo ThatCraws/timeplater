ab75066b18c3240e2d11fcf532aea46680d1ad45
Julien <julien.krause@protonmail.com>
2024-02-17 01:47:44 +0100

    Began implementation of WorkWeek-model

--- src/main/java/de/esterlino/timeplater/worktimes/model/WorkDay.java
+++ src/main/java/de/esterlino/timeplater/worktimes/model/WorkDay.java
@@ -1,5 +1,109 @@
 package de.esterlino.timeplater.worktimes.model;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class WorkDay {
+    private final LocalTime startTimeHome;
+    private final LocalTime endTimeHome;
+    private final LocalTime startTimeOffice;
+    private final LocalTime endTimeOffice;
+
+    private final boolean breakAtHome;
+    /**
+     * The break made that day in minutes.
+     */
+    private int breakTime;
+
+    private DayOfWeek dayOfWeek;
+
+    /**
+     * This constructor constructs a {@code WorkDay}, where I switched either from
+     * home office to office-office or from office-office to home-office.
+     * <br>
+     * <br>
+     * The {@code breakTime} will be set solely by duration of work-time. If it
+     * differs, set retroactively via {@link setBreakTime}.
+     * 
+     * @param dayOfWeek       The day of the week this work-day was
+     * @param startTimeHome   Starting time of home office
+     * @param endTimeHome     Ending time of home office
+     * @param startTimeOffice Starting time of office-office
+     * @param endTimeOffice   Ending time of office-office
+     * @param breakAtHome     If true, the break will be counted towards the home
+     *                        office. If false, the break will count towards the
+     *                        office-office
+     */
+    public WorkDay(
+            final DayOfWeek dayOfWeek,
+            final LocalTime startTimeHome, final LocalTime endTimeHome,
+            final LocalTime startTimeOffice, final LocalTime endTimeOffice,
+            final boolean breakAtHome) {
+
+        this.dayOfWeek = dayOfWeek;
+        this.startTimeHome = startTimeHome;
+        this.endTimeHome = endTimeHome;
+        this.startTimeOffice = startTimeOffice;
+        this.endTimeOffice = endTimeOffice;
+        this.breakAtHome = breakAtHome;
+    }
+
+    /**
+     * This constructor constructs a WorkDay, where I either worked only in the
+     * home-office or only in the office-office.
+     * <br>
+     * <br>
+     * The {@code breakTime} will be set solely by duration of work-time. If it
+     * differs, set retroactively via {@link setBreakTime}.
+     * 
+     * @param dayOfWeek   The day of the week this work-day was
+     * @param startTime   Time of starting work
+     * @param endTime     Time of ending work
+     * @param breakAtHome If true, the whole day was spent in home-office. If
+     *                    false, the whole day was spent in office-office.
+     */
+    public WorkDay(final DayOfWeek dayOfWeek, final LocalTime startTime, final LocalTime endTime,
+            final boolean breakAtHome) {
+        this.dayOfWeek = dayOfWeek;
+        this.breakAtHome = breakAtHome;
+        if (breakAtHome) {
+            this.startTimeHome = startTime;
+            this.endTimeHome = endTime;
+            this.startTimeOffice = null;
+            this.endTimeOffice = null;
+        } else {
+            this.startTimeHome = null;
+            this.endTimeHome = null;
+            this.startTimeOffice = startTime;
+            this.endTimeOffice = endTime;
+        }
+    }
+
+    // Setters / Getters
+    public void setBreakTime(final int breakTime) {
+        this.breakTime = breakTime;
+    }
+
+    public DayOfWeek getDayOfWeek() { 
+        return dayOfWeek;
+    }
+
+    public Duration getWorkTime() {
+        if (isWorkFromHomeOnly()) {
+            return Duration.between(endTimeHome, startTimeHome);
+        } else {
+            // TODO: Check if it was a hybrid-day!!!
+            return Duration.between(endTimeOffice, startTimeOffice);
+        }
+    }
+
+    /**
+     * 
+     * @return True, if I worked from home <b>only</b> that day
+     */
+    private boolean isWorkFromHomeOnly() {
+        return startTimeOffice == null || endTimeOffice == null;
+    }
 
 }
--- src/main/java/de/esterlino/timeplater/worktimes/model/WorkWeek.java
+++ src/main/java/de/esterlino/timeplater/worktimes/model/WorkWeek.java
@@ -1,5 +1,37 @@
 package de.esterlino.timeplater.worktimes.model;
 
-public class WorkWeek {
+import java.util.logging.Logger;
 
+public class WorkWeek {
+    private final WorkDay[] workDays;
+
+    /**
+     * Constructs a {@code WorkWeek} with the given array. The array should have size 5 and reflect the working days (so index 0 is Mo and 4 is Fr).
+     * @param workDays Array of {@Code WorkDays}
+     */
+    public WorkWeek(final WorkDay[] workDays) {
+        if (workDays.length != 5) {
+            Logger.getLogger(WorkWeek.class.getName() + ".constructor(WorkDay[])").warning("Constructing WorkWeek with an array of WorkDays, that has a size different than 5");
+        }
+        this.workDays = workDays;
+    }
+
+    /**
+     * Constructs an empty {@code WorkWeek}.
+     */
+    public WorkWeek() {
+        this.workDays = new WorkDay[5];
+    }
+
+    /**
+     * Adds a {@code WorkDay} to this {@code WorkWeek}. Will replace {@code WorkDays} of the same day of the week, if present.
+     * @param workDay The {@code WorkDay} to add to this {@code WorkWeed}
+     * @return The {@code WorkDay} that previously set for this day of the week (null, if none!)
+     */
+    public WorkDay addWorkDay(final WorkDay workDay) {
+        int indexDayOfWeek = workDay.getDayOfWeek().getValue() - 1;
+        WorkDay previouslySetForDayOfWeek = workDays[indexDayOfWeek];
+        workDays[indexDayOfWeek] = workDay;
+        return previouslySetForDayOfWeek;
+    }
 }
