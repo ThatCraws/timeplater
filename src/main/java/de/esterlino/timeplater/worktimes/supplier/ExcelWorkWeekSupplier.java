064e03b973069aaf5218d82f0e2622e07e5b0824
Julien <julien.krause@protonmail.com>
2024-02-21 21:10:44 +0100

    Dummy ExcelWorkWeekSupplier (for test to run at least)

--- src/main/java/de/esterlino/timeplater/worktimes/supplier/ExcelWorkWeekSupplier.java
+++ src/main/java/de/esterlino/timeplater/worktimes/supplier/ExcelWorkWeekSupplier.java
@@ -1,13 +1,39 @@
 package de.esterlino.timeplater.worktimes.supplier;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
 import de.esterlino.timeplater.worktimes.model.WorkWeek;
 
 public class ExcelWorkWeekSupplier implements WorkWeekSupplier {
+    private final Workbook workTimeWorkBook;
+
+    public ExcelWorkWeekSupplier(final String excelPath) {
+        FileInputStream excelFileInputStream = null;
+        try {
+            excelFileInputStream = new FileInputStream(excelPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.err);
+        }
+        workTimeWorkBook = null;
+        try {
+            if (excelFileInputStream != null) {
+                excelFileInputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
 
     @Override
     public WorkWeek supplyWorkWeek(final int calendarWeek) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'supplyWorkWeek'");
+        // throw new UnsupportedOperationException("Unimplemented method 'supplyWorkWeek'");
+        return new WorkWeek(1);
     }
 
 }
