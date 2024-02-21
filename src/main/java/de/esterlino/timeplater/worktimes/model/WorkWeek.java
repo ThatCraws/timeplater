c509e8027284ce650dec02ac79df75cbe7490e82
Julien <julien.krause@protonmail.com>
2024-02-21 19:05:42 +0100

    Using HashMap for WorkWeek now

--- src/main/java/de/esterlino/timeplater/worktimes/model/WorkWeek.java
+++ src/main/java/de/esterlino/timeplater/worktimes/model/WorkWeek.java
@@ -1,37 +1,89 @@
 package de.esterlino.timeplater.worktimes.model;
 
-import java.util.logging.Logger;
+import java.time.DayOfWeek;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * Represents one set of working days for a week.
+ * 
+ * @author <a href=julien.krause@pm.me>Julien</a>
+ */
 public class WorkWeek {
-    private final WorkDay[] workDays;
+    private final int calendarWeek;
+    private final Map<DayOfWeek, WorkDay> workDays;
 
     /**
-     * Constructs a {@code WorkWeek} with the given array. The array should have size 5 and reflect the working days (so index 0 is Mo and 4 is Fr).
-     * @param workDays Array of {@Code WorkDays}
+     * Constructs a {@link WorkWeek} with the given array of {@code WorkWeeks}.
+     * It will be iterated and duplicate values for the
+     * {@link WorkDay#getDayOfWeek()}
+     * will override each other. So the last instance of each {@link DayOfWeek} will
+     * be used.
+     * 
+     * @param workDayArray Array of {@code WorkDays}
      */
-    public WorkWeek(final WorkDay[] workDays) {
-        if (workDays.length != 5) {
-            Logger.getLogger(WorkWeek.class.getName() + ".constructor(WorkDay[])").warning("Constructing WorkWeek with an array of WorkDays, that has a size different than 5");
+    public WorkWeek(final int calendarWeek, final WorkDay[] workDayArray) {
+        this.calendarWeek = calendarWeek;
+        this.workDays = new HashMap<>();
+
+        for (WorkDay workDay : workDayArray) {
+            workDays.put(workDay.getDayOfWeek(), workDay);
         }
-        this.workDays = workDays;
     }
 
     /**
-     * Constructs an empty {@code WorkWeek}.
+     * Constructs an empty {@link WorkWeek}.
      */
-    public WorkWeek() {
-        this.workDays = new WorkDay[5];
+    public WorkWeek(final int calendarWeek) {
+        this(calendarWeek, new WorkDay[] {});
     }
 
     /**
-     * Adds a {@code WorkDay} to this {@code WorkWeek}. Will replace {@code WorkDays} of the same day of the week, if present.
-     * @param workDay The {@code WorkDay} to add to this {@code WorkWeed}
-     * @return The {@code WorkDay} that previously set for this day of the week (null, if none!)
+     * Adds a {@link WorkDay} to this {@link WorkWeek}. If {@code replace} is
+     * {@code true} (default), will replace {@code WorkDays} of the same
+     * {@code dayOfTheWeek}.
+     * 
+     * @param workDay The {@link WorkDay} to add to this {@link WorkWeek}
+     * @param replace If true, replace {@code WorkDays} referring to the same
+     *                {@code dayOfWeek}. If false, will do nothing, if
+     *                {@code workDay} with same {@code dayOfTheWeek} exists already.
+     *                <b>Default: {@code true}</b>
+     * @return The {@link WorkDay} that was previously set for the day of the week
+     *         of the added {@link WorkDay}.
+     *         Will be {@code null}, if none pre-existed or replace is {@code false}
+     *         and
+     *         {@link WorkDay} of the same day of the week did already exist
+     */
+    public WorkDay addWorkDay(final WorkDay workDay, final boolean replace) {
+        if (!replace && workDays.containsKey(workDay.getDayOfWeek())) {
+            return null;
+        }
+        return workDays.put(workDay.getDayOfWeek(), workDay);
+    }
+
+    /**
+     * Adds a {@link WorkDay} to this {@link WorkWeek}. Will replace
+     * {@code WorkDays} of the same day of the week, if present.
+     * 
+     * @param workDay The {@link WorkDay} to add to this {@link WorkWeek}
+     * @return The {@link WorkDay} that previously set for this day of the week
+     *         (null, if none!)
      */
     public WorkDay addWorkDay(final WorkDay workDay) {
-        int indexDayOfWeek = workDay.getDayOfWeek().getValue() - 1;
-        WorkDay previouslySetForDayOfWeek = workDays[indexDayOfWeek];
-        workDays[indexDayOfWeek] = workDay;
-        return previouslySetForDayOfWeek;
+        return addWorkDay(workDay, true);
+    }
+
+    /**
+     * Returns the {@link WorkDay}, that is set for the given {@link DayOfWeek}
+     * 
+     * @param dayOfWeek
+     * @return
+     */
+    public WorkDay getWorkDay(final DayOfWeek dayOfWeek) {
+        return workDays.get(dayOfWeek);
+    }
+
+    public int getCalendarWeek() {
+        return calendarWeek;
     }
 }
