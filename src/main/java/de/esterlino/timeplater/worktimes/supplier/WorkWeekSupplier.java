f857ceece150d0279bd7d5626c4d8109c32b8ec2
Julien <julien.krause@protonmail.com>
2024-02-24 23:12:10 +0100

    JavaDoc'd

--- src/main/java/de/esterlino/timeplater/worktimes/supplier/WorkWeekSupplier.java
+++ src/main/java/de/esterlino/timeplater/worktimes/supplier/WorkWeekSupplier.java
@@ -2,6 +2,20 @@ package de.esterlino.timeplater.worktimes.supplier;
 
 import de.esterlino.timeplater.worktimes.model.WorkWeek;
 
+/**
+ * This {@code Interface} is used to supply {@link WorkWeek}-instances to the
+ * {@code TimePlater}. Implementing classes decide the source of the
+ * {@link WorkWeek}.
+ */
 public interface WorkWeekSupplier {
+    /**
+     * Return the {@link WorkWeek} of the given calendar-week.
+     * 
+     * @param calendarWeek The calendar-week of which to construct and return a
+     *                     {@link WorkWeek}-object of
+     * @return The {@link WorkWeek}, containing the respective
+     *         {@link de.esterlino.timeplater.worktimes.model.WorkTime}s of the
+     *         given calendar-week
+     */
     public WorkWeek supplyWorkWeek(final int calendarWeek);
 }
