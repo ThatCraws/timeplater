b3a93888a1dd42a3d8cee5d59ba2e9b43c21e655
Julien <jukra_100@yahoo.de>
2024-03-09 01:45:46 +0100

    Minimal command-line application

--- src/main/java/de/esterlino/timeplater/TimeplaterApplication.java
+++ src/main/java/de/esterlino/timeplater/TimeplaterApplication.java
@@ -4,14 +4,27 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
+import de.esterlino.timeplater.worktimes.ouputter.MailTemplateWorkWeekOutputter;
+import de.esterlino.timeplater.worktimes.supplier.ExcelWorkWeekSupplier;
+import de.esterlino.timeplater.worktimes.supplier.ExcelWorkbookInitializationException;
+
 public class TimeplaterApplication {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My App");
-        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        if (args.length < 2) {
+            System.err.println("Too few arguments. Please enter: inputFile calendarWeek [outputFile]");
+            return;
+        }
 
-        SwingUtilities.invokeLater(() -> {
-            frame.setVisible(true);
-        });
+        ExcelWorkWeekSupplier supplier = null;
+        try {
+            supplier = new ExcelWorkWeekSupplier(args[0]);
+        } catch (ExcelWorkbookInitializationException e) {
+            e.printStackTrace(System.err);
+            return;
+        }
+        
+        MailTemplateWorkWeekOutputter outputter = new MailTemplateWorkWeekOutputter(args.length >= 3 ? args[2] : "output.txt");
+        outputter.createOutput(supplier.supplyWorkWeek(Integer.parseInt(args[1])));
     }
 }
